0ee7a40378f4b3e94474bb8bd583d9df40c1e934
Erin Davidson <rosemary.erin.o@gmail.com>
2022-12-05 16:03:56 +0000

    finish exercise 12

--- the_basics/ex12.js
+++ the_basics/ex12.js
@@ -1,5 +1,10 @@
-// What does the following expression evaluate to?
-'12' < '9'
-/*
-When JavaScript compares strings
-*/
+//What does the following expression evaluate to?
+'12' < '9'; // === true
+/* The expression returns true. Strings use lexicographic ordering.
+Lexicographic ordering is:
+  1) Dictionary order except ALL Uppercase letters preceed lowercase letters.
+  *is compared character by character, the first occurance of two characters
+  not matching determines what the expression evaluates to.
+  *if all characters in a string match, then the shorter string is 'less than'
+  the larger string.
+  */
