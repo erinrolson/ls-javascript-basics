207d318729a34fb6c0ef1d9833149d2aabfcd704
Erin Davidson <rosemary.erin.o@gmail.com>
2022-12-14 21:37:42 +0000

    variable scope exercise

---
+++ JS100/variableScope/valuePt1.js
@@ -0,0 +1,7 @@
+console.log(greeting);
+
+var greeting = 'Hello world!';
+/*
+because greeting is initialized with var it is hoisted with the value of
+undefined until the program reaches line 3 and is initialized to a string value.
+*/
