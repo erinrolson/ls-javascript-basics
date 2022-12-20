05d1261e21d2f8e173ca2bbe41de7912078f2390
Erin Davidson <rosemary.erin.o@gmail.com>
2022-12-20 17:52:43 +0000

    Functions 2 exercises

---
+++ JS100/functions2/arrowFuncPt2.js
@@ -0,0 +1,22 @@
+/* refactor the code using arrow function syntax
+
+let initGame = function () {
+  return {
+    level: 1,
+    score: 0
+  }
+};
+
+let game = initGame();
+
+console.log('Level: ' + game.level);
+console.log('Score: ' + game.score);
+*/
+
+let initGame = () => ( {level: 1, score: 0,} );
+
+let game = initGame();
+
+console.log('Level: ' + game.level);
+console.log('Score: ' + game.score);
+
--- JS100/functions2/catAge.js
+++ JS100/functions2/catAge.js
@@ -6,5 +6,22 @@ calculated as follows:
 - all other years = 4 cat years
 */
 function catAge(years){
-  
+  if (years === 0) { return 0 }
+  let catsAge = 0;
+  for (let i = years; i >= 1; i -= 1){
+    if ( i > 2){
+      catsAge += 4;
+    } else if (i === 2){
+      catsAge += 9;
+    } else if ( i === 1){
+      catsAge += 15;
+      return catsAge;
+    }
+  }
 }
+
+console.log(catAge(0)); // 0
+console.log(catAge(1)); // 15
+console.log(catAge(2)); // 24
+console.log(catAge(3)); // 28
+console.log(catAge(4)); // 32
