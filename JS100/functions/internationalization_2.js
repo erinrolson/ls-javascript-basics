130f5f4b137a0e09e794c433ee67a5240e1c5128
Erin Davidson <rosemary.erin.o@gmail.com>
2022-12-20 18:24:22 +0000

    Solve the exercise

--- JS100/functions/internationalization_2.js
+++ JS100/functions/internationalization_2.js
@@ -2,6 +2,50 @@
 write a function localGreet that takes a locale as input, 
 and returns a greeting
 */
-function localGreet() {
+function greet(code){
   
+  switch (code) {
+    case 'en' :
+      return 'Hello';
+    case 'af' :
+      return 'Haai';
+    case 'fr':
+      return 'Salut';
+    default :
+      return 'not a valid language code.';
+  }
 }
+
+function extractLanguage(string) {
+  return string.split('_')[0];
+}
+
+function extractRegion(string) {
+  return string.split('_')[1]
+               .slice( 0, 2);
+}
+
+function localGreet(locale) {
+  let region = extractRegion(locale);
+  let language = extractLanguage(locale);
+  switch (region) {
+    case 'US':
+      return 'Hey!';
+    case 'GB':
+      return 'Hello!';
+    case 'AU':
+      return 'Howdy';
+    
+    default:
+      return greet(language);
+  }
+}
+
+console.log(localGreet('en_US.UTF-8')); // 'Hey!'
+console.log(localGreet('en_GB.UTF-8')); // 'Hello!'
+console.log(localGreet('en_AU.UTF-8')); // 'Howdy!'
+
+console.log(localGreet('fr_FR.UTF-8')); // 'Salut!'
+console.log(localGreet('fr_CA.UTF-8')); // 'Salut!'
+console.log(localGreet('fr_MA.UTF-8')); // 'Salut!'
+
